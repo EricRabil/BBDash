31594b7e2b040e1186bf64687f0c0267ba6ea955
Eric Rabil <ericjrabil@gmail.com>
2021-03-05 12:44:47 -0500

    Added experimental localStorage caching of results until initial data loads

--- packages/app/src/composables/_query.ts
+++ packages/app/src/composables/_query.ts
@@ -1,3 +1,18 @@
 import { QueryClient } from "react-query";
+import { createLocalStoragePersistor } from "react-query/createLocalStoragePersistor-experimental";
+import { persistQueryClient } from "react-query/persistQueryClient-experimental";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+        },
+    }
+});
+
+const localStoragePersistor = createLocalStoragePersistor();
+ 
+persistQueryClient({
+    queryClient,
+    persistor: localStoragePersistor,
+});
