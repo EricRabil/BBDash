41345244e6696cd5e4710b4426077ac73d62d764
Eric Rabil <ericjrabil@gmail.com>
2021-04-21 15:07:49 -0400

    Display course ID instead of name when holding shift

--- packages/app/src/components/context-menu/items/CourseColorCoding.tsx
+++ packages/app/src/components/context-menu/items/CourseColorCoding.tsx
@@ -2,12 +2,14 @@ import { Course } from "@bbdash/shared";
 import React, { PropsWithoutRef, useCallback, useContext } from "react";
 import { CirclePicker, ColorResult } from "react-color";
 import { ColorCodingContext } from "../../../contexts/color-coding-context";
+import { ModifierKeyContext } from "../../../contexts/modifier-key-context";
 
 /**
  * ContextMenu item for changing the color of a course
  */
 export default function CourseColorCoding({ course }: PropsWithoutRef<{ course: Course }>) {
     const { courseColors, setCourseColorPreference, colors } = useContext(ColorCodingContext);
+    const { shift } = useContext(ModifierKeyContext);
 
     const colorChanged = useCallback(({ hex }: ColorResult) => {
         setCourseColorPreference(course.id, colors.indexOf(hex));
@@ -15,7 +17,9 @@ export default function CourseColorCoding({ course }: PropsWithoutRef<{ course: 
 
     return (
         <div className="course-color-picker">
-            <div className="course-color-picker--course-name">{ course.displayName || course.name }</div>
+            <div className="course-color-picker--course-name">{
+                shift ? course.id : (course.displayName || course.name)
+            }</div>
 
             <CirclePicker colors={colors} color={courseColors[course.id]} circleSpacing={7} circleSize={21} width={"252px"} onChangeComplete={colorChanged}></CirclePicker>
         </div>
