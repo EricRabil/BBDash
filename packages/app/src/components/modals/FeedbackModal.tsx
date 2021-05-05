684897e59a1a4968cd2491d066de84dee8ffcdd6
Eric Rabil <ericjrabil@gmail.com>
2021-05-05 13:41:40 -0400

    Implemented feedback API so their feedback actually goes somewhere

--- packages/app/src/components/modals/FeedbackModal.tsx
+++ packages/app/src/components/modals/FeedbackModal.tsx
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useToasts } from "react-toast-notifications";
+import { ModalContext } from "../../contexts/modal-context";
 import BBModal, { BBModalContentContext } from "./BBModal";
 
 export default function BugReportModal(props: Omit<BBModalContentContext, "children">) {
+    const formRef = useRef<HTMLFormElement>();
+    const { addToast } = useToasts();
+
     return (
-        <BBModal {...props} header={
-            <span>Feedback Assistant</span>
-        } type="form">
-            <form>
-                <label className="input-group">
-                    <span className="input-header">Email</span>
-                    <input type="email" />
-                </label>
-                <label className="input-group">
-                    <span className="input-header">Feedback</span>
-                    <textarea />
-                </label>
-            </form>
-        </BBModal>
+        <ModalContext.Consumer>
+            {({ toggleShowing, setBusy }) => (
+                <BBModal {...props} header={
+                    <span>Feedback Assistant</span>
+                } onDismiss={async () => {
+                    const formData = new FormData(formRef.current!);
+
+                    try {
+                        setBusy(true);
+
+                        await fetch("https://api.bbdash.app/api/v1/feedback", {
+                            method: "POST",
+                            body: new URLSearchParams(formData as unknown as string)
+                        });
+
+                        addToast((
+                            <div>Thanks for your feedback. We&apos;ll reach out to you if we need more information.</div>
+                        ), {
+                            appearance: "success",
+                            autoDismiss: true,
+                            id: "feedback-notice"
+                        });
+                    } catch(e) {
+                        addToast((
+                            <div>Sorry, we couldn&apos;t send your feedback. Please try again later.</div>
+                        ), {
+                            appearance: "error",
+                            autoDismiss: true,
+                            id: "feedback-notice"
+                        });
+                    } finally {
+                        setBusy(false);
+                        toggleShowing();
+                    }
+                }} type="form">
+                    <form ref={e => formRef.current = e!}>
+                        <label className="input-group">
+                            <span className="input-header">Email</span>
+                            <input name="email" type="email" />
+                        </label>
+                        <label className="input-group">
+                            <span className="input-header">Title</span>
+                            <input name="title" type="text" />
+                        </label>
+                        <label className="input-group">
+                            <span className="input-header">Feedback</span>
+                            <textarea name="feedback" />
+                        </label>
+                    </form>
+                </BBModal>
+            )}
+        </ModalContext.Consumer>
     );
 }
